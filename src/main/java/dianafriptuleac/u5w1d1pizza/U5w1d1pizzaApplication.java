a6201afe21d90a17e43a2bed43e635c8dd08c57d
Diana <dianadorojuc3@gmail.com>
2024-10-21 13:46:31 +0200

    Initial commit

---
+++ src/main/java/dianafriptuleac/u5w1d1pizza/U5w1d1pizzaApplication.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w1d1pizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w1d1pizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w1d1pizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dianafriptuleac/u5w1d1pizza/U5w1d1pizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package dianafriptuleac.u5w1d1pizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w1d1pizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
